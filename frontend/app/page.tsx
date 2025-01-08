5e42a7559a9dfafe6020ce467098db51ceefb1bb
Raj Thombare <therajthombare@gmail.com>
2025-01-08 13:25:53 +0530

    initial commit

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>hi</h1>
+      <Button>button test asdad</Button>
+    </div>
+  );
+}
