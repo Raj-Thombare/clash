5eac588bc20fb717af9ab5b24c051930871626a0
Raj Thombare <therajthombare@gmail.com>
2025-01-11 13:23:02 +0530

    update landing, auth pages

--- frontend/app/(auth)/register/page.tsx
+++ frontend/app/(auth)/register/page.tsx
@@ -1,72 +1,97 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import Link from "next/link";
+"use client";
 import React from "react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import {
+  IconBrandGithub,
+  IconBrandGoogle,
+  IconBrandOnlyfans,
+} from "@tabler/icons-react";
 
-type Props = {};
-
-function page({}: Props) {
+const page = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <div className='w-[550px] bg-white rounded-xl py-5 px-10 shadow-md'>
-        <h1 className='text-4xl font-extrabold bg-gradient-to-r from-pink-400 to-purple-500 text-transparent bg-clip-text text-center'>
-          Clash
-        </h1>
-        <h1 className='text-3xl font-bold mt-2'>Register</h1>
-        <p>Welcome back</p>
-        <form
-          action='
-    '>
-          <div className='mt-4'>
-            <Label htmlFor='name'>Name</Label>
-            <Input
-              id='name'
-              name='name'
-              type='text'
-              placeholder='Enter your name...'
-            />
+    <div className='h-screen flex justify-center items-center p-8 md:p-0'>
+      <div className='mt-0 md:mt-20 max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black'>
+        <h2 className='font-bold text-2xl text-center text-neutral-800 dark:text-neutral-200'>
+          Welcome to Clash
+        </h2>
+        <form className='my-8' onSubmit={handleSubmit}>
+          <div className='flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4'>
+            <LabelInputContainer>
+              <Label htmlFor='firstname'>First name</Label>
+              <Input id='firstname' placeholder='Tyler' type='text' />
+            </LabelInputContainer>
+            <LabelInputContainer>
+              <Label htmlFor='lastname'>Last name</Label>
+              <Input id='lastname' placeholder='Durden' type='text' />
+            </LabelInputContainer>
           </div>
-          <div className='mt-4'>
-            <Label htmlFor='email'>Email</Label>
-            <Input
-              id='email'
-              name='email'
-              type='email'
-              placeholder='Enter your email...'
-            />
-          </div>
-          <div className='mt-4'>
+          <LabelInputContainer className='mb-4'>
+            <Label htmlFor='email'>Email Address</Label>
+            <Input id='email' placeholder='projectmayhem@fc.com' type='email' />
+          </LabelInputContainer>
+          <LabelInputContainer className='mb-4'>
             <Label htmlFor='password'>Password</Label>
+            <Input id='password' placeholder='••••••••' type='password' />
+          </LabelInputContainer>
+          <LabelInputContainer className='mb-8'>
+            <Label htmlFor='twitterpassword'>Confirm Password</Label>
             <Input
-              id='password'
-              name='password'
-              type='password'
-              placeholder='Enter your password...'
+              id='twitterpassword'
+              placeholder='••••••••'
+              type='twitterpassword'
             />
-          </div>
-          <div className='mt-4'>
-            <Label htmlFor='password'>Confirm Password</Label>
-            <Input
-              id='cpassword'
-              name='confirm_password'
-              type='password'
-              placeholder='Confirm your password...'
-            />
-          </div>
-          <div className='mt-4'>
-            <Button className='w-full'>Submit</Button>
+          </LabelInputContainer>
+
+          <button
+            className='bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]'
+            type='submit'>
+            Register &rarr;
+          </button>
+
+          <div className='bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full' />
+
+          <div className='flex flex-col space-y-4'>
+            <button
+              className=' relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]'
+              type='submit'>
+              <IconBrandGithub className='h-4 w-4 text-neutral-800 dark:text-neutral-300' />
+              <span className='text-neutral-700 dark:text-neutral-300 text-sm'>
+                GitHub
+              </span>
+            </button>
+            <button
+              className=' relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]'
+              type='submit'>
+              <IconBrandGoogle className='h-4 w-4 text-neutral-800 dark:text-neutral-300' />
+              <span className='text-neutral-700 dark:text-neutral-300 text-sm'>
+                Google
+              </span>
+            </button>
           </div>
         </form>
-        <p className='mt-2 text-center'>
-          Already have an account ?{" "}
-          <strong>
-            <Link href='/login'>Login</Link>
-          </strong>
-        </p>
       </div>
     </div>
   );
-}
+};
+
+const LabelInputContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("flex flex-col space-y-2 w-full", className)}>
+      {children}
+    </div>
+  );
+};
 
 export default page;
--- frontend/components/base/HeroSection.tsx
+++ frontend/components/base/HeroSection.tsx
@@ -2,24 +2,34 @@ import React from "react";
 import Image from "next/image";
 import { Button } from "../ui/button";
 import Link from "next/link";
+import { HoverBorderGradient } from "../ui/hover-border-gradient";
+import { ShootingStars } from "../ui/shooting-stars";
+import { StarsBackground } from "../ui/stars-background";
+import { TextHoverEffect } from "../ui/text-hover-effect";
 
 type Props = {};
 
 const HeroSection = (props: Props) => {
   return (
-    <div className='w-full h-screen flex justify-center items-center flex-col'>
+    <div className='w-full h-screen rounded-md bg-neutral-900 flex flex-col items-center justify-center relative'>
       <div>
-        <Image src='/banner.svg' alt='banner_image' width={600} height={600} />
+        <div className='gap-y-5'>
+          <TextHoverEffect text='Clash' />
+          <h3 className='relative flex-col md:flex-row z-10 text-3xl md:text-5xl md:leading-tight max-w-5xl mx-auto text-center tracking-tight font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white flex items-center gap-2 md:gap-8'>
+            Discover the better choice, together!
+          </h3>
+        </div>
+        <ShootingStars />
+        <StarsBackground />
       </div>
-      <div className='text-center mt-5'>
-        <h1 className='text-6xl md:text-7xl lg:text-9xl font-extrabold bg-gradient-to-r from-pink-400 to-purple-500 text-transparent bg-clip-text'>
-          Clash
-        </h1>
-        <p className='text-2xl md:text-3xl lg:text-4xl font-bold text-center mt-2'>
-          Discover the better choice, together!
-        </p>
+      <div className='mt-10'>
         <Link href='/login'>
-          <Button className='mt-5'>START FREE</Button>
+          <HoverBorderGradient
+            containerClassName='rounded-full'
+            as='button'
+            className='flex items-center space-x-2'>
+            <span>START FREE</span>
+          </HoverBorderGradient>
         </Link>
       </div>
     </div>
