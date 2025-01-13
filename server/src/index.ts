477b8591344e1cf8ce2861ef4fbbbc87c9b61bf4
Raj Thombare <therajthombare@gmail.com>
2025-01-13 22:07:15 +0530

    setup views

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,25 @@
 import express, { Application, Request, Response } from "express";
 import "dotenv/config"
+import path from "path";
+import { fileURLToPath } from "url";
+
 const app: Application = express();
 
 const PORT = process.env.PORT || 7000;
 
-app.get('/', (req, res) => {
-    res.send("Hey it's working...")
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.set("view engine", "ejs");
+
+app.set("views", path.resolve(__dirname, "./views"));
+
+app.get('/', (req: Request, res: Response) => {
+    res.render("welcome");
 })
 
 app.listen(PORT), () => {
