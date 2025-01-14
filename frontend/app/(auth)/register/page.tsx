62c2a670618de73496364062d0381cb0aca227a1
Raj Thombare <therajthombare@gmail.com>
2025-01-14 22:57:44 +0530

    remove register form field

--- frontend/app/(auth)/register/page.tsx
+++ frontend/app/(auth)/register/page.tsx
@@ -21,19 +21,13 @@ const page = () => {
           Welcome to Clash
         </h2>
         <form className='my-8' onSubmit={handleSubmit}>
-          <div className='flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4'>
-            <LabelInputContainer>
-              <Label htmlFor='firstname'>First name</Label>
-              <Input id='firstname' placeholder='Tyler' type='text' />
-            </LabelInputContainer>
-            <LabelInputContainer>
-              <Label htmlFor='lastname'>Last name</Label>
-              <Input id='lastname' placeholder='Durden' type='text' />
-            </LabelInputContainer>
-          </div>
+          <LabelInputContainer className='mb-4'>
+            <Label htmlFor='name'>Full Name</Label>
+            <Input id='name' placeholder='John Doe' type='text' />
+          </LabelInputContainer>
           <LabelInputContainer className='mb-4'>
             <Label htmlFor='email'>Email Address</Label>
-            <Input id='email' placeholder='projectmayhem@fc.com' type='email' />
+            <Input id='email' placeholder='johndoe@test.com' type='email' />
           </LabelInputContainer>
           <LabelInputContainer className='mb-4'>
             <Label htmlFor='password'>Password</Label>
